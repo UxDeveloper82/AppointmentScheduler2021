c2e839c861c744e70186de6d4e477e71ffb11144
UxDeveloper82 <electronicsmf@aol.com>
2021-09-15 22:19:40 -0400

    Appointment Setup

---
+++ Models/ViewModels/PatientVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models.ViewModels
+{
+    public class PatientVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
