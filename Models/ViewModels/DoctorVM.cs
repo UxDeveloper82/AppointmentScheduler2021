9ec6b6be605c795b54293e049e4a57b60c1c49a5
UxDeveloper82 <electronicsmf@aol.com>
2021-09-15 22:57:14 -0400

    Appointment Service Get Doctor Part 1

--- Models/ViewModels/DoctorVM.cs
+++ Models/ViewModels/DoctorVM.cs
@@ -8,7 +8,7 @@ namespace AppointmentScheduler.Models.ViewModels
 {
     public class DoctorVM
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string  Name { get; set; }
     }
